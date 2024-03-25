a92cb51a57bb9ff81f9d0582fa7489f8385655cc
krol santa <csantasa@cesde.net>
2024-03-25 14:03:29 -0500

    actividad2

---
+++ app2.py
@@ -0,0 +1,167 @@
+#Actividad #2 Crear un proyecto de Python en Visual Studio Code
+#Nombre : carolina restrepo santa
+#Semestre = #3 bello noche
+
+#Variables
+"""
+#Int = Conversión de un número en coma flotante a entero
+
+#Ejemplo #1
+#result = int(9.9)
+#print('int(9.9):', result) 
+# Convertir un número de coma flotante en su entero equivalente
+
+#Ejemplo #2
+#Conversión de una cadena a entero con la Función int() en Python
+#numero_texto = "123"
+#numero_entero = int(numero_texto)
+#print(numero_entero)
+#En este ejemplo, hemos convertido la cadena «123» en un número entero utilizando int().
+
+#Ejemplo #3
+#Conversión en una base diferente
+#numero_binario = "1101"
+#numero_decimal = int(numero_binario, 2)
+#print(numero_decimal) 
+#Aquí, hemos convertido el número binario «1101» en su equivalente decimal utilizando la base 2 como segundo argumento de int().
+
+#Ejemplo #4
+#hexadecimal = '1A'  # Representación hexadecimal de 26
+#decimal_desde_hexadecimal = int(hexadecimal, 16)
+#print(decimal_desde_hexadecimal)  # Imprimirá "26"
+
+#Ejemplo #5
+#import sys
+#numero_grande = 5**10000
+#print(sys.getsizeof(numero_grande))  # Imprimirá el tamaño en bytes
+
+#Str = Cómo crear una cadena en Python
+
+#Ejemplo #1
+#s = 'Hola Pythonista'
+#s
+#'Hola Pythonista'
+#type(s)
+#s2 = "Me gusta Python"
+#s2
+#'Me gusta Python'
+#type(s2)
+#print(s,s2)
+
+#Ejemplo #2
+#cadena = 'Hola mundo '
+#indice = cadena.find('mundo')
+#print(indice)  # Resultado: 5 (índice donde comienza la subcadena 'mundo')
+
+#Ejemplo #3
+#numero = 42
+#cadena_numero = str(numero)
+#print(cadena_numero)  # Salida: "42"
+#numero_decimal = 3.14159234
+#cadena_decimal = str(numero_decimal)
+#print(cadena_decimal)  # Salida: "3.14159"
+
+#Ejemplo #4
+#s = "Primer linea\nSegunda linea"
+#print(s)
+#Primer linea
+#Segunda linea
+
+#Ejemplo #5
+#s = "Los números son:\n{a} y {b}".format(a=5, b=10)
+#print(s) #Los números son 5 y 10
+
+#float = 
+
+#Ejemplo #1
+#numero_entero = 10
+#numero_flotante = float(numero_entero)
+#print(numero_flotante)  # Salida 10.0
+
+#Ejemplo #2
+#numero_decimal = 3.1416
+#print("Número decimal:", numero_decimal)
+# Ejemplo #3
+#numero_entero = 7
+#numero_flotante = float(numero_entero)
+#print("Número flotante:", numero_flotante)
+# Ejemplo #4
+#numero_cientifico = 1.5e-3
+#print("Número científico:", numero_cientifico)
+# Ejemplo #5
+#suma = numero_decimal + numero_flotante
+#resta = numero_decimal - numero_cientifico
+#print("Suma:", suma)
+#print("Resta:", resta)
+
+"""
+#Operadores
+"""
+a = 10
+b = 5
+suma = a + b
+print("Suma:", suma)  # Resultado: 15
+resta = a - b
+print("Resta:", resta)  # Resultado: 5
+multiplicacion = a * b
+print("Multiplicación:", multiplicacion)  # Resultado: 50
+division = a / b
+print("División:", division)  # Resultado: 2.0
+modulo = a % b
+print("Módulo:", modulo)  # Resultado: 0
+exponente = a ** b
+print("Exponente:", exponente)  # Resultado: 100000
+"""
+#Estructuras condicionales
+"""
+# Sentencia if
+#x = 5
+#if x > 4:
+#    print("¡La condición era verdadera!") #Esta sentencia se ejecuta
+
+# Sentencia else
+#y = 3
+#if y > 4:
+#    print("¡No voy a imprimir!") #esta sentencia no se ejecuta
+#else:
+#    print("¡La condición no era verdadera!") #esta sentencia se ejecuta
+
+# Sentencia elif
+#z = 7
+#if z > 8:
+#    print("¡No voy a imprimir!") #esta sentencia no se ejecuta
+#elif z > 5:
+#    print("¡Yo lo haré!") #esta sentencia se ejecuta
+#elif z > 6:
+#    print("¡Tampoco voy a imprimir!") #esta sentencia no se ejecuta
+#else:   
+#    print("¡Yo tampoco!") #esta sentencia no se ejecuta
+"""
+#Bucles
+"""
+#bucle while
+#def run():
+#    LIMITE = 10000
+#    contador = 0
+#    potencia_2 = 2** contador
+#    while potencia_2 < LIMITE:
+#        print('2 Elevado a ' + str(contador) + ' es igual a: ' + str(potencia_2))
+#        contador = contador + 1
+#        potencia_2 = 2** contador      
+#if __name__ == '__main__':
+#    run()
+
+#bucle for
+#def run():
+#    for contador in range(1, 1002):
+#        print(contador)
+#if __name__ == '__main__':
+#    run()
+"""
+
+
+
+
+
+
+
